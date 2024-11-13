66ecbb8a881c4bda420a5d9840d631a4ea0a175d
sahil dudhat <sahildudhat03@gmail.com>
2024-11-13 11:41:19 +0530

    change time

--- server.js
+++ server.js
@@ -97,90 +97,108 @@ const test = async () => {
 // const schedule = "0 4 * * *"; // UK cron job at 5 AM
 
 const timezone = "Asia/Kolkata"; // Set timezone to IST
-const schedule = "35 11 * * *"; // Runs at 11:35 AM IST
+const schedule = "45 11 * * *"; // Runs at 11:45 AM IST
+// cron.schedule(
+//   schedule,
+//   async () => {
+//     try {
+//       console.log("Running a task every day at 5 AM UK time");
+//       const csvFilePathBranchA = path.join(__dirname, "./BranchA.csv");
+//       const csvFilePathBranchB = path.join(__dirname, "./BranchB.csv");
+//       const csvFilePathBranchC = path.join(__dirname, "./BranchC.csv");
+//       const csvFilePathBranchD = path.join(__dirname, "./BranchD.csv");
+
+//       if (fs.existsSync(csvFilePathBranchA)) {
+//         fs.unlinkSync(csvFilePathBranchA);
+//         console.log("Existing BranchA file removed.");
+//       }
+//       if (fs.existsSync(csvFilePathBranchB)) {
+//         fs.unlinkSync(csvFilePathBranchB);
+//         console.log("Existing BranchB file removed.");
+//       }
+//       if (fs.existsSync(csvFilePathBranchC)) {
+//         fs.unlinkSync(csvFilePathBranchC);
+//         console.log("Existing BranchC file removed.");
+//       }
+//       if (fs.existsSync(csvFilePathBranchD)) {
+//         fs.unlinkSync(csvFilePathBranchD);
+//         console.log("Existing BranchD file removed.");
+//       }
+
+//       await fetchCategoryTree();
+//       await new Promise((resolve) => setTimeout(resolve, 5000));
+
+//       const readCategoryTreeResult = await readCategoryTree();
+//       console.log(
+//         "readCategoryTreeResult.length :>> ",
+//         readCategoryTreeResult.length
+//       );
+
+//       let index = 0;
+//       for (const i1 of readCategoryTreeResult) {
+//         if (i1.name != "Tool Hire" && i1.name != "Benchmarx Kitchens") {
+//           console.log("i1-->", i1.name);
+//           if (i1.subCategories.length > 0) {
+//             for (const i2 of i1.subCategories) {
+//               if (i2.subCategories.length > 0) {
+//                 for (const i3 of i2.subCategories) {
+//                   await BranchA(i3, i1.name);
+//                   await BranchB(i3, i1.name);
+//                   await BranchC(i3, i1.name);
+//                   await BranchD(i3, i1.name);
+
+//                   if (i3.subCategories.length > 0) {
+//                     for (const i4 of i3.subCategories) {
+//                       if (i4.subCategories.length > 0) {
+//                         await BranchA(i4, i1.name);
+//                         await BranchB(i4, i1.name);
+//                         await BranchC(i4, i1.name);
+//                         await BranchD(i4, i1.name);
+//                       }
+//                     }
+//                   }
+//                 }
+//               }
+//             }
+//             index++;
+//             console.log("*=*=*=*=*=*=*=*=*=*=*=*= :>> ", index);
+//           }
+//         }
+//       }
+//       MerchantsA();
+//       MerchantsB();
+//       MerchantsC();
+//       MerchantsD();
+//       console.log(
+//         "\x1b[32m*=*=*=*=*=*=*=*=*=*/\x1b[0m \x1b[31mAll Products successfully Imported.\x1b[0m \x1b[32m/*=*=*=*=*=*=*=*=*=*\x1b[0m"
+//       );
+//     } catch (error) {
+//       console.error("Error during initial execution:", error);
+//     }
+//   },
+//   {
+//     timezone: timezone, // Set timezone to UK time
+//   }
+// );
+
 cron.schedule(
   schedule,
-  // async () => {
-  //   try {
-  //     console.log("Running a task every day at 5 AM UK time");
-  //     const csvFilePathBranchA = path.join(__dirname, "./BranchA.csv");
-  //     const csvFilePathBranchB = path.join(__dirname, "./BranchB.csv");
-  //     const csvFilePathBranchC = path.join(__dirname, "./BranchC.csv");
-  //     const csvFilePathBranchD = path.join(__dirname, "./BranchD.csv");
-
-  //     if (fs.existsSync(csvFilePathBranchA)) {
-  //       fs.unlinkSync(csvFilePathBranchA);
-  //       console.log("Existing BranchA file removed.");
-  //     }
-  //     if (fs.existsSync(csvFilePathBranchB)) {
-  //       fs.unlinkSync(csvFilePathBranchB);
-  //       console.log("Existing BranchB file removed.");
-  //     }
-  //     if (fs.existsSync(csvFilePathBranchC)) {
-  //       fs.unlinkSync(csvFilePathBranchC);
-  //       console.log("Existing BranchC file removed.");
-  //     }
-  //     if (fs.existsSync(csvFilePathBranchD)) {
-  //       fs.unlinkSync(csvFilePathBranchD);
-  //       console.log("Existing BranchD file removed.");
-  //     }
-
-  //     await fetchCategoryTree();
-  //     await new Promise((resolve) => setTimeout(resolve, 5000));
-
-  //     const readCategoryTreeResult = await readCategoryTree();
-  //     console.log(
-  //       "readCategoryTreeResult.length :>> ",
-  //       readCategoryTreeResult.length
-  //     );
-
-  //     let index = 0;
-  //     for (const i1 of readCategoryTreeResult) {
-  //       if (i1.name != "Tool Hire" && i1.name != "Benchmarx Kitchens") {
-  //         console.log("i1-->", i1.name);
-  //         if (i1.subCategories.length > 0) {
-  //           for (const i2 of i1.subCategories) {
-  //             if (i2.subCategories.length > 0) {
-  //               for (const i3 of i2.subCategories) {
-  //                 await BranchA(i3, i1.name);
-  //                 await BranchB(i3, i1.name);
-  //                 await BranchC(i3, i1.name);
-  //                 await BranchD(i3, i1.name);
-
-  //                 if (i3.subCategories.length > 0) {
-  //                   for (const i4 of i3.subCategories) {
-  //                     if (i4.subCategories.length > 0) {
-  //                       await BranchA(i4, i1.name);
-  //                       await BranchB(i4, i1.name);
-  //                       await BranchC(i4, i1.name);
-  //                       await BranchD(i4, i1.name);
-  //                     }
-  //                   }
-  //                 }
-  //               }
-  //             }
-  //           }
-  //           index++;
-  //           console.log("*=*=*=*=*=*=*=*=*=*=*=*= :>> ", index);
-  //         }
-  //       }
-  //     }
-  //     MerchantsA();
-  //     MerchantsB();
-  //     MerchantsC();
-  //     MerchantsD();
-  //     console.log(
-  //       "\x1b[32m*=*=*=*=*=*=*=*=*=*/\x1b[0m \x1b[31mAll Products successfully Imported.\x1b[0m \x1b[32m/*=*=*=*=*=*=*=*=*=*\x1b[0m"
-  //     );
-  //   } catch (error) {
-  //     console.error("Error during initial execution:", error);
-  //   }
-  // },
-  test(),
+  async () => {
+    try {
+      console.log("Running a task every day at 5 AM UK time");
+      test();
+      console.log(
+        "\x1b[32m*=*=*=*=*=*=*=*=*=*/\x1b[0m \x1b[31mAll Products successfully Imported.\x1b[0m \x1b[32m/*=*=*=*=*=*=*=*=*=*\x1b[0m"
+      );
+    } catch (error) {
+      console.error("Error during initial execution:", error);
+    }
+  },
   {
     timezone: timezone, // Set timezone to UK time
   }
 );
+
 // test();
 app.get("/", (req, res) => {
   res.status(200).json({ message: "WelCome - V1.0.1" });
